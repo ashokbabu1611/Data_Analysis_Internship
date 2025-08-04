3f575b2f8852d92a4691b154356df09e3cb3c7ab
ashokbabu1611 <chappidiashokbabu1611@gmail.com>
2025-08-04 18:46:09 +0530

    Create OutputTask1.py

---
+++ OutputTask1.py
@@ -0,0 +1,11 @@
+  name  age gender        city     score
+  Alice   25 female    New York 85.000000
+    Bob   27   male Los Angeles 78.000000
+Charlie   30 female     Chicago 81.857143
+  David   22   male     Unknown 65.000000
+    Eve   27 female       Miami 92.000000
+  Frank   27   male     Houston 81.857143
+  Grace   29 female      Dallas 88.000000
+ Hannah   27 female   San Diego 90.000000
+    Ian   31   male     Unknown 75.000000
+  Julia   24 female      Boston 81.857143
